763ab006e67437b3685d18a13c03f83a8aa4c020
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-09-26 15:11:18 +0430

    Changes made in footer

--- front-end/src/layouts/admin/Footer.jsx
+++ front-end/src/layouts/admin/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="py-4 bg-light mt-auto">
             <div className="container-fluid px-4">
                 <div className="d-flex align-items-center justify-content-between small">
-                    <div className="text-muted">Copyright &copy; Your Website 2023</div>
+                    {/* <div className="text-muted">Copyright &copy; Your Website 2023</div> */}
                     <div>                                <a href="#">Privacy Policy</a>
                         &middot;
                         <Link to="#">Terms &amp; Conditions</Link>
