4b98122b63ce644fb6867241eea0b16cff47c855
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-09-26 15:12:06 +0430

    Footer error removed

--- front-end/src/layouts/admin/Footer.jsx
+++ front-end/src/layouts/admin/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
         <footer className="py-4 bg-light mt-auto">
             <div className="container-fluid px-4">
                 <div className="d-flex align-items-center justify-content-between small">
-                    {/* <div className="text-muted">Copyright &copy; Your Website 2023</div> */}
-                    <div>                                <a href="#">Privacy Policy</a>
+                    <div className="text-muted">Copyright &copy; Your Website 2023</div>
+                    <div>                                <Link to="#">Privacy Policy</Link>
                         &middot;
                         <Link to="#">Terms &amp; Conditions</Link>
                     </div>
